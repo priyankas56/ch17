e7f5944f7d167a0940d66e1b7732e503cae920ca
Sowmyagowda11 <145636361+Sowmyagowda11@users.noreply.github.com>
2024-07-24 09:57:02 +0530

    objects

---
+++ objects/HelicopterRunner.java
@@ -0,0 +1,74 @@
+class HelicopterRunner
+{
+	public static void main(String[] values)
+	{
+		System.out.println("start main in helicopterrunner");
+		 Helicopter helicopter=new Helicopter();
+		 Helicopter helicopter1=new Helicopter();
+		 Helicopter helicopter2=new Helicopter();
+		 Helicopter helicopter3=new Helicopter();
+		 Helicopter helicopter4=new Helicopter();
+		 
+		 Mall mall=new Mall();
+		 Mall mall1=new Mall();
+		 Mall mall2=new Mall();
+		 Mall mall3=new Mall();
+		 Mall mall4=new Mall();
+		 
+		 Market market=new Market();
+		 Market market1=new Market();
+		 Market market2=new Market();
+		 Market market3=new Market();
+		 Market market4=new Market();
+		 
+		 Train train=new Train();
+		 Train train1=new Train();
+		 Train train2=new Train();
+		 Train train3=new Train();
+		 Train train4=new Train();
+		 
+		 Ring ring=new Ring();
+		 Ring ring1=new Ring();
+		 Ring ring2=new Ring();
+		 Ring ring3=new Ring();
+		 Ring ring4=new Ring();
+		 
+		 Auto auto=new Auto();
+		 Auto auto1=new Auto();
+		 Auto auto2=new Auto();
+		 Auto auto3=new Auto();
+		 Auto auto4=new Auto();
+		 
+		 Auditorium auditorium=new Auditorium();
+		 Auditorium auditorium1=new Auditorium();
+		 Auditorium auditorium2=new Auditorium();
+		 Auditorium auditorium3=new Auditorium();
+		 Auditorium auditorium4=new Auditorium();
+		 
+		 SpaceShip spaceship=new SpaceShip();
+		 SpaceShip spaceship1=new SpaceShip();
+		 SpaceShip spaceship2=new SpaceShip();
+		 SpaceShip spaceship3=new SpaceShip();
+		 SpaceShip spaceship4=new SpaceShip();
+		 
+		 BlackHole blackhole=new BlackHole();
+		 BlackHole blackhole1=new BlackHole();
+		 BlackHole blackhole2=new BlackHole();
+		 BlackHole blackhole3=new BlackHole();
+		 BlackHole blackhole4=new BlackHole();
+		 
+		 Cake cake=new Cake();
+		 Cake cake1=new Cake();
+		 Cake cake2=new Cake();
+		 Cake cake3=new Cake();
+		 Cake cake4=new Cake();
+		 
+		 Wrapper wrapper=new Wrapper();
+		 Wrapper wrapper1=new Wrapper();
+		 Wrapper wrapper2=new Wrapper();
+		 Wrapper wrapper3=new Wrapper();
+		 Wrapper wrapper4=new Wrapper();
+		 
+		System.out.println("end main in helicopterrunner");	
+	}
+}
---
+++ objects/Mall.java
@@ -0,0 +1,104 @@
+class Mall
+{
+	public Mall()
+	{
+		System.out.println("created mall");
+	}
+}
+class Train
+{
+	public Train()
+	{
+		System.out.println("created train");
+	}
+}
+class Ring
+{
+	public Ring()
+	{
+		System.out.println("created ring");
+	}
+}
+class Auto
+{
+	public Auto()
+	{
+		System.out.println("created auto");
+	}
+}
+class Auditorium
+{
+	public Auditorium()
+	{
+		System.out.println("created Auditorium");
+	}
+}
+class SpaceShip
+{
+	public SpaceShip()
+	{
+		System.out.println("created SpaceShip");
+	}
+}
+class Alien
+{
+	public Alien()
+	{
+		System.out.println("created Alien");
+	}
+}
+class Bullet
+{
+	public Bullet()
+	{
+		System.out.println("created Bullet");
+	}
+}
+class Galaxy
+{
+	public Galaxy()
+	{
+		System.out.println("created Galaxy");
+	}
+}
+class Spoon
+{
+	public Spoon()
+	{
+		System.out.println("created spoon");
+	}
+}
+class BlackHole
+{
+	public BlackHole()
+	{
+		System.out.println("created BlackHole");
+	}
+}
+class Cake
+{
+	public Cake()
+	{
+		System.out.println("created cake");
+	}
+}
+class Wrapper
+{
+	public Wrapper()
+	{
+		System.out.println("created Wrapper");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
